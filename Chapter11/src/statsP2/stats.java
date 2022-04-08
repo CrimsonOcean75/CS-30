8884fc771f7132c6750657bf1208312275443c21
904261003 <904261003@L815WD220026.edu.cbe.ab.ca>
2022-04-08 10:07:06 -0600

    stats almost work

--- Chapter11/src/statsP2/stats.java
+++ Chapter11/src/statsP2/stats.java
@@ -30,12 +30,12 @@ public class stats {
 		 String userfile ;
 		 boolean writing = true;
 		 int todd =1 ;
-		 
+		 int numbwerof ;
 		 
         //foramt for the file to be read must be name first then score
 		 try {
 		
-		 int highLow[] = {}; //array to be appened by the users imputs
+		 int highLow[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //array to be appened by the users imputs
 		 
 		 
 		 //make the file and put the values in
@@ -66,7 +66,7 @@ public class stats {
 	        }
 		 
 		 
-		 boolean flag = false; 
+		
 		 String name_sco = "";
 		 int n = 0;
 		 
@@ -76,7 +76,11 @@ public class stats {
 		 writeFile = new BufferedWriter(out);
 		 pw = new PrintWriter(writeFile);
 		 
-		 while (writing == true) {
+		 System.out.println("enter the number of students you want to enter");
+		 numbwerof = input.nextInt();
+		 input.nextLine(); //eating \n
+		 
+		 while (n < numbwerof*2) { //while the loop counter is lesthen 2 times the number of students (2 x because you need to write scores and names) 
 		 
 			 
 			 
@@ -84,7 +88,7 @@ public class stats {
 			 
 			 
 			 if (n %2 == 0) { //runs this one first 
-				 flag = false; 
+				 
 				 System.out.println("enter the students name: ");
 				 name_sco = input.nextLine();
 				 pw.println(name_sco); //writing to the file
@@ -97,21 +101,9 @@ public class stats {
 				 name_sco = input.nextLine();
 				 pw.println(name_sco); //writing to the file
 				 pw.flush();
-				 flag = true;
-			 }
-			 
-			 
-			 
-			 if (flag == true){
-				 System.out.println("if you want to stop adding data enter 1, enter any other number to continue");
-				 one = input.nextInt();
-				 input.nextLine(); //eats \n
-				 
-				 if (one == 1) {
-					 writing = false; //if the user wants to end it ends the loop 
-				 }
 				 
 			 }
+			 
 			 
 			 n = n + 1; 
 			 
@@ -129,7 +121,7 @@ public class stats {
 		 while ((score = readFile.readLine()) != null ) { //read the file and do the averages and stuff
 			 
 		
-			 if(count % 2 ==0 ) { //if numScores is even
+			 if(count % 2 ==0 ) { //if count is even
 			 
 				 totalScores += Double.parseDouble(score); //adding to the total score if the line is a score line
 		 
@@ -138,7 +130,7 @@ public class stats {
 		 
 				 System.out.println(score); //print ln to get to next line
 				 numScore += 1;
-				 highLow[count] = (int)Double.parseDouble(score); 
+				 highLow[count] = ((int)Double.parseDouble(score)); 
 			 }
 		 
 			 else {
@@ -154,9 +146,30 @@ public class stats {
 		
 		 Arrays.sort(highLow); //sort the users imputs 
 		 
+		 boolean goouasg = true;
+		 
+		 n=0;
+		 
+		 
+		 
+		 while (goouasg) {
+			 
+			 if (highLow[n] != 0 && n < highLow.length  ) {
+				 
+				 if(goouasg == false) {
+				 System.out.println("Minimum = " + highLow[n]);
+				 }
+				 else
+				 {
+					 System.out.println("Minimum = " + "0");
+				 }
+				 goouasg = false;
+			 }
+			 
+			 n += 1;
+		 }
 		
-		
-		 System.out.println("Minimum = " + highLow[0]); //print the lowest 
+		 
 		 System.out.println("Maximum = " + highLow[highLow.length-1]); //print the highest 
 
 		 
