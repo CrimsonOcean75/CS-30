8f7e00d5f5e3257b7f9670fcd05e11bb9a03db65
904261003 <904261003@L815WD220026.edu.cbe.ab.ca>
2022-04-11 10:12:20 -0600

    stats 2 working

--- Chapter11/src/statsP2/stats.java
+++ Chapter11/src/statsP2/stats.java
@@ -1,3 +1,19 @@
+/*
+
+Program: stats.java          Last Date of this Revision: april 12, 2022
+
+Purpose: An application that creates a file based on user input then writes user inputed names and scores to a txt file
+
+Author: Max Piercey, 
+School: CHHS
+Course: Computer Programming 30
+
+
+*/
+
+
+
+
 package statsP2;
 
 import java.io.BufferedReader;
@@ -16,26 +32,28 @@ public class stats {
 
 	public static void main(String[] args) {
 		 Scanner input = new Scanner(System.in);
+		 
 		 FileReader in;
 		 BufferedReader readFile;
 		 FileWriter out;
 		 BufferedWriter writeFile;
 		 PrintWriter pw;
-		 int one ;
+		 
+		 
 		 String score; // a string for the line that comes in that will be converted into a double
 		 double avgScore; // what will reperesnt the average
 		 double totalScores = 0; //total of all scores in file
 		 int count = 1; // number of scores in file
 		 int numScore=0;
 		 String userfile ;
-		 boolean writing = true;
 		 int todd =1 ;
 		 int numbwerof ;
+		
 		 
         //foramt for the file to be read must be name first then score
 		 try {
 		
-		 int highLow[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //array to be appened by the users imputs
+		 int highLow[] = {}; //array to be appened by the users imputs
 		 
 		 
 		 //make the file and put the values in
@@ -80,6 +98,10 @@ public class stats {
 		 numbwerof = input.nextInt();
 		 input.nextLine(); //eating \n
 		 
+		 int[] arrNew = new int[numbwerof];
+		 int startFromTheBottemNowWeHere = 0;
+		 
+		 
 		 while (n < numbwerof*2) { //while the loop counter is lesthen 2 times the number of students (2 x because you need to write scores and names) 
 		 
 			 
@@ -91,6 +113,7 @@ public class stats {
 				 
 				 System.out.println("enter the students name: ");
 				 name_sco = input.nextLine();
+				 
 				 pw.println(name_sco); //writing to the file
 				 pw.flush();
 				 
@@ -99,14 +122,15 @@ public class stats {
 			  if (n % 2 != 0 ){ //runs this one second then alternates between them as n is incremented
 				 System.out.println("enter the students score: ");
 				 name_sco = input.nextLine();
+				 arrNew [highLow.length + startFromTheBottemNowWeHere] = (int)Double.parseDouble(name_sco); //it's like god wrote this code
 				 pw.println(name_sco); //writing to the file
 				 pw.flush();
+				 startFromTheBottemNowWeHere = startFromTheBottemNowWeHere+ 1;
 				 
 			 }
 			 
-			 
+			  
 			 n = n + 1; 
-			 
 			 
 		 }
 		 
@@ -130,7 +154,7 @@ public class stats {
 		 
 				 System.out.println(score); //print ln to get to next line
 				 numScore += 1;
-				 highLow[count] = ((int)Double.parseDouble(score)); 
+				 
 			 }
 		 
 			 else {
@@ -144,33 +168,12 @@ public class stats {
 		 avgScore = totalScores / numScore;
 		 System.out.println("Average was: " + avgScore);
 		
-		 Arrays.sort(highLow); //sort the users imputs 
+		 Arrays.sort(arrNew); //sort the users imputs 
 		 
-		 boolean goouasg = true;
-		 
-		 n=0;
-		 
-		 
-		 
-		 while (goouasg) {
-			 
-			 if (highLow[n] != 0 && n < highLow.length  ) {
-				 
-				 if(goouasg == false) {
-				 System.out.println("Minimum = " + highLow[n]);
-				 }
-				 else
-				 {
-					 System.out.println("Minimum = " + "0");
-				 }
-				 goouasg = false;
-			 }
-			 
-			 n += 1;
-		 }
+		 System.out.println("Minimum = " + arrNew[0]);
 		
 		 
-		 System.out.println("Maximum = " + highLow[highLow.length-1]); //print the highest 
+		 System.out.println("Maximum = " + arrNew[arrNew.length-1]); //print the highest 
 
 		 
 		 writeFile.close();
@@ -184,6 +187,49 @@ public class stats {
 			 System.out.println("Problem reading file.");
 			 System.err.println("IOException: " + e.getMessage());
 		 }
+		 input.close();
 	}
 
 }
+
+/* screen dump
+ * 
+ * 
+ * 
+ * enter the file that you want to right score values to
+Gloobed
+File created
+enter the number of students you want to enter
+4
+enter the students name: 
+tom
+enter the students score: 
+50
+enter the students name: 
+jimbo
+enter the students score: 
+100
+enter the students name: 
+billy
+enter the students score: 
+50
+enter the students name: 
+jermimia
+enter the students score: 
+100
+tom got a: 50
+jimbo got a: 100
+billy got a: 50
+jermimia got a: 100
+Average was: 75.0
+Minimum = 50
+Maximum = 100
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
